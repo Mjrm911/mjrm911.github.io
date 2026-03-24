2dff64f909bdd530712d77838203318b2e4cdb06
Mjrm911 <156038099+Mjrm911@users.noreply.github.com>
2026-03-24 13:52:28 +0300

    Refactor push event listener in sw.js
    Removed notification display logic from push event listener.

--- sw.js
+++ sw.js
@@ -1,11 +1,1 @@
-self.addEventListener('push', function(event) {
-    const data = event.data.json();
-    const options = {
-        body: data.body,
-        icon: 'icon.png', // حط أي صورة تبيها أيقونة
-        badge: 'icon.png'
-    };
-    event.waitUntil(
-        self.registration.showNotification(data.title, options)
-    );
-});
+self.addEventListener('push', function(event) { /* انتظر الإشعارات */ });
